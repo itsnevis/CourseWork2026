6a34c2cf7bd70eed364e0918b911211a2865328d
Казинцев Максим <kazintsev.m.a@edu.mirea.ru>
2026-05-14 02:06:17 +0300

    5454

--- app/src/main/java/com/mirea/kt/ribo/kazintsev/AuthActivity.java
+++ app/src/main/java/com/mirea/kt/ribo/kazintsev/AuthActivity.java
@@ -6,15 +6,21 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import com.android.volley.NoConnectionError;
-import com.android.volley.Request;
-import com.android.volley.TimeoutError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+
 import com.google.android.material.textfield.TextInputEditText;
+import com.mirea.kt.ribo.kazintsev.R;
+
 import org.json.JSONObject;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class AuthActivity extends AppCompatActivity {
     @Override
@@ -26,52 +32,71 @@ public class AuthActivity extends AppCompatActivity {
         TextInputEditText etPassword = findViewById(R.id.etPassword);
         TextInputEditText etGroup = findViewById(R.id.etGroup);
         Button btn = findViewById(R.id.btnLogin);
-        etGroup.setText("РИБО-04-24");
+        etGroup.setText("RIBO-04-24");
 
         btn.setOnClickListener(v -> {
             String l = etLogin.getText().toString().trim();
             String p = etPassword.getText().toString().trim();
             String g = etGroup.getText().toString().trim();
 
-            // ПРОБУЕМ HTTP (без S), так как сервер учебный и SSL может глючить
-            String url = "http://android-for-students.ru/coursework/login.php";
+            String url = "https://android-for-students.ru/coursework/login.php";
+            OkHttpClient client = new OkHttpClient();
+            RequestBody formBody = new FormBody.Builder()
+                    .add("lgn", l)
+                    .add("pwd", p)
+                    .add("g", g)
+                    .build();
 
-            StringRequest sr = new StringRequest(Request.Method.POST, url,
-                    res -> {
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(formBody)
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    runOnUiThread(() -> {
+                        Log.e("AUTH_ERR", e.toString());
+                        Toast.makeText(
+                                AuthActivity.this,
+                                "Ошибка сети: " + e.getMessage(),
+                                Toast.LENGTH_LONG
+                        ).show();
+                    });
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    String res = response.body().string();
+                    runOnUiThread(() -> {
                         try {
+                            Log.d("SERVER_RESPONSE", res);
                             JSONObject jo = new JSONObject(res);
-                            if (jo.getInt("result_code") == 1) {
-                                Intent i = new Intent(this, TaskActivity.class);
+                            if (jo.getInt("result_code") == 0) {
+                                Intent i = new Intent(AuthActivity.this, TaskActivity.class);
                                 i.putExtra("variant", jo.getString("variant"));
                                 i.putExtra("task", jo.getString("task"));
                                 startActivity(i);
                                 finish();
                             } else {
-                                Toast.makeText(this, "Ошибка сервера: " + jo.optInt("result_code"), Toast.LENGTH_SHORT).show();
+                                Toast.makeText(
+                                        AuthActivity.this,
+                                        "Ошибка сервера: "
+                                                + jo.optInt("result_code"),
+                                        Toast.LENGTH_LONG
+                                ).show();
                             }
                         } catch (Exception e) {
-                            Log.e("AUTH_ERR", "Response: " + res);
-                            Toast.makeText(this, "Ошибка данных: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            Log.e("JSON_ERR", res);
+                            Toast.makeText(
+                                    AuthActivity.this,
+                                    "Ошибка JSON: " + e.getMessage(),
+                                    Toast.LENGTH_LONG
+                            ).show();
                         }
-                    },
-                    err -> {
-                        String message = "Ошибка сети";
-                        if (err instanceof TimeoutError) message = "Сервер не отвечает (Тайм-аут)";
-                        else if (err instanceof NoConnectionError) message = "Нет интернета на устройстве";
-
-                        Log.e("AUTH_ERR", "Detail: " + err.toString());
-                        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-                    }) {
-                @Override
-                protected Map<String, String> getParams() {
-                    Map<String, String> map = new HashMap<>();
-                    map.put("lgn", l);
-                    map.put("pwd", p);
-                    map.put("g", g);
-                    return map;
+                    });
                 }
-            };
-            Volley.newRequestQueue(this).add(sr);
+            });
         });
     }
 }
